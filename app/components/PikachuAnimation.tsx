cd33d7dcdbc3962bcc4aefbfbe18be7bf8f9405f
srivastavya02 <srivastavya24@gmail.com>
2026-01-27 05:02:23 +0530

    add evee and squirtle

--- app/components/PikachuAnimation.tsx
+++ app/components/PikachuAnimation.tsx
@@ -2,24 +2,51 @@
 
 import { useState } from "react";
 
+type Pokemon = "pikachu" | "eevee" | "squirtle";
+
 export default function PikachuAnimation() {
-  const [isJumping, setIsJumping] = useState(false);
+  const [currentPokemon, setCurrentPokemon] = useState<Pokemon>("pikachu");
+  const [isLeaving, setIsLeaving] = useState(false);
+  const [isEntering, setIsEntering] = useState(false);
 
   const handleClick = () => {
-    setIsJumping(true);
-    setTimeout(() => setIsJumping(false), 600);
+    setIsLeaving(true);
+
+    setTimeout(() => {
+      setCurrentPokemon(prev => {
+        if (prev === "pikachu") return "eevee";
+        if (prev === "eevee") return "squirtle";
+        return "pikachu";
+      });
+      setIsLeaving(false);
+      setIsEntering(true);
+    }, 300);
+
+    setTimeout(() => {
+      setIsEntering(false);
+    }, 800);
+  };
+
+  const pokemonSprites = {
+    pikachu: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif",
+    eevee: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/133.gif",
+    squirtle: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif"
   };
 
   return (
     <div
-      className={`fixed bottom-4 right-4 cursor-pointer z-40 transition-transform duration-300 ${
-        isJumping ? "-translate-y-16" : "hover:-translate-y-1"
+      className={`fixed bottom-4 right-4 cursor-pointer z-40 transition-all duration-300 ${
+        isLeaving
+          ? "-translate-x-24 opacity-0"
+          : isEntering
+            ? "translate-x-0 opacity-100"
+            : "hover:-translate-y-1"
       }`}
       onClick={handleClick}
     >
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/25.gif"
-        alt="Pikachu"
+        src={pokemonSprites[currentPokemon]}
+        alt={currentPokemon}
         className="w-20 h-20"
         style={{ imageRendering: "pixelated" }}
       />
