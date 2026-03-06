4f513729fb8070bdd3a8b2ab595be79419747364
jerkeyray <jerkeyray@users.noreply.github.com>
2026-03-07 03:16:59 +0530

    feat: add bg to blog

--- app/components/FloralOverlay.tsx
+++ app/components/FloralOverlay.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-
 export default function FloralOverlay() {
-  const pathname = usePathname();
-
-  // Hide on blog reader pages (blog/[slug]), show everywhere else
-  const isBlogReader = pathname.startsWith("/blog/");
-  if (isBlogReader) return null;
-
   return (
     <div
       className="pointer-events-none fixed inset-0 z-[60] opacity-[0.012]"
